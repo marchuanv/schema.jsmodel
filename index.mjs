5605f8ca1f641661d027a776b875ca330b6cc119
marchuanv <marchuanv@gmail.com>
2024-07-07 00:29:30 +0200

    fixes

--- index.mjs
+++ index.mjs
@@ -1,5 +1,17 @@
 import { ModelClasses } from "./lib/model-classes.mjs";
 import { SchemaLoader } from "./lib/schema-loader.mjs";
-const schemaLoader = new SchemaLoader('./spec/schemas/person.schema.json');
-await schemaLoader.load();
-ModelClasses.create(schemaLoader);
+import { SchemaModel } from "./lib/schema-model.mjs";
+import { existsSync } from 'node:fs';
+/**
+ * @returns { Array<SchemaModel> }
+*/
+export async function getModels(schemaFilePath) {
+    if (!existsSync(schemaFilePath)) {
+        throw new Error(`${schemaFilePath} file not found.`);
+    }
+    const schemaLoader = new SchemaLoader(schemaFilePath);
+    await schemaLoader.load();
+    const exportFilePath = ModelClasses.create(schemaLoader);
+    const { default: exports } = await import(exportFilePath);
+    return exports;
+}
