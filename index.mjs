b836329d3ef9aab4ff7f5194254a7a9ee32f998d
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-07-18 22:50:55 +0200

    refactor

--- index.mjs
+++ index.mjs
@@ -1,19 +1,12 @@
 import { pathToFileURL } from 'node:url';
-
 import { ModelClasses } from './lib/model-classes.mjs';
 import { SchemaLoader } from './lib/schema-loader.mjs';
-import { SchemaModel } from './lib/schema-model.mjs';
 /**
- * @param { SchemaLoader } schemaLoader
- * @returns { Array<{ type: class, instance: SchemaModel }> }
+ * @param { String } jsonSchemaFilePath
 */
-export async function getModels(schemaLoader) {
-    if (schemaLoader === null || schemaLoader === undefined || !(schemaLoader instanceof SchemaLoader)) {
-        throw new Error(`The schemaLoader argument is null, undefined or not an instance of ${SchemaLoader.name}`);
-    }
+export async function getModels(jsonSchemaFilePath) {
+    const schemaLoader = new SchemaLoader(jsonSchemaFilePath);
     await schemaLoader.load();
     const exportFilePath = ModelClasses.create(schemaLoader);
     return await import(pathToFileURL(exportFilePath));
 }
-export { SchemaLoader, SchemaModel };
-
