8826c2d58c64e79a9770c547a021eec509f75f33
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-07-18 23:33:45 +0200

    trying exports

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import { pathToFileURL } from 'node:url';
 import { ModelClasses } from './lib/model-classes.mjs';
 import { SchemaLoader } from './lib/schema-loader.mjs';
+import { SchemaModel } from './lib/schema-model.mjs';
 const jsonSchemaFilePath = process.argv[2];
 (async () => {
     const schemaLoader = new SchemaLoader(jsonSchemaFilePath);
@@ -8,3 +9,5 @@ const jsonSchemaFilePath = process.argv[2];
     const exportFilePath = ModelClasses.create(schemaLoader);
     return await import(pathToFileURL(exportFilePath));
 })().catch(err => console.log(err));
+export { SchemaModel };
+
