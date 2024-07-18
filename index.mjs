5e4e4d5812bd04ffed4e8c785a9a4101a82dd467
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-07-18 23:40:11 +0200

    trying exports

--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@ import { ModelClasses } from './lib/model-classes.mjs';
 import { SchemaLoader } from './lib/schema-loader.mjs';
 import { SchemaModel } from './lib/schema-model.mjs';
 (async () => {
-    const schemaInfoKeys = Object(schemaInfo);
+    const schemaInfoKeys = Object.keys(schemaInfo);
     for (const key of schemaInfoKeys) {
         const { jsonSchemaFilePath } = schemaInfo[key];
         const schemaLoader = new SchemaLoader(jsonSchemaFilePath);
