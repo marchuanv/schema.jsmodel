4d1add05c13be5bd9aa37a586563be67464ba6e1
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-07-18 23:30:46 +0200

    refactor

--- index.mjs
+++ index.mjs
@@ -2,11 +2,9 @@ import { pathToFileURL } from 'node:url';
 import { ModelClasses } from './lib/model-classes.mjs';
 import { SchemaLoader } from './lib/schema-loader.mjs';
 const jsonSchemaFilePath = process.argv[2];
-const schemaLoader = new SchemaLoader(jsonSchemaFilePath);
-await schemaLoader.load();
-const exportFilePath = ModelClasses.create(schemaLoader);
-const exported = await import(pathToFileURL(exportFilePath));
-const keys = Object.keys(exported);
-for (const key of keys) {
-    module.exports[key] = exported[key];
-}
+(async () => {
+    const schemaLoader = new SchemaLoader(jsonSchemaFilePath);
+    await schemaLoader.load();
+    const exportFilePath = ModelClasses.create(schemaLoader);
+    return await import(pathToFileURL(exportFilePath));
+})().catch(err => console.log(err));
